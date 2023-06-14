53a0f396f862f6f06a39aefcc59fed6951058b61
ldebele <lemidebele@gmail.com>
2023-06-14 14:23:49 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -2,28 +2,15 @@ from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.readlines()
-    requirements = [lib.replace('\n', '') for lib in requirements]
+    requirements = [r.replace('\n', '') for r in requirements]
 
 
+# creating a setup file for the project.
 setup(
     name="Employee_Attrition_Prediction",
     version="1.0",
-    description="",
     author="Lemi Debele",
     author_email="lemidebele@gmail.com",
     packages=find_packages(),
-
     install_requires=requirements,
-
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-
 )
