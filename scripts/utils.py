d4b75f563dbaff921306a96235750080512c4fce
ldebele <lemidebele@gmail.com>
2023-06-20 00:13:14 +0300

    analyze the data

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,23 @@ def plot_groupby(data, index, values):
 
     # groupby and plot
     data.groupby([index, values]).size().unstack().plot(kind='bar', stacked=True, ax=ax)
+
+
+
+def grid_plot_groupby(data, list_columns, values, ncols=1, nrows=1, l=10, w=5):
+    """
+        parameters:
+            data: pd.DataFrame
+            list_columns: list
+            values: str
+            ncols: int
+            nrows: int
+            l: int
+            w: int
+    """
+
+    fig, ax = plt.subplots(figsize=(l, w), ncols=ncols, nrows=nrows)
+    ax = ax.flatten()
+
+    for i, col in enumerate(list_columns):
+        data.groupby([col, values]).size().unstack().plot(kind="bar", stacked=True, ax=ax[i])
